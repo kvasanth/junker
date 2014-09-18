a2fcb8762006719971c90dca1201b3de8be72202
Ameet Sangamesh <sameet@lpt-rharshavardhan.(none)>
2014-09-18 18:32:51 +0530

    some random commiy

--- pyth.py
+++ pyth.py
@@ -1,48 +1,5 @@
-mega Inc license
-# Copyright (c) 2003-2014 ZeOmega Inc
-# http://www.zeomega.com
-# All rights reserved.
-#
-# ZeOmega software [both binary and source (if released)] (hereafter,
-# Software) is intellectual property owned by ZeOmega Inc is copyright
-# of ZeOmega Inc in all countries in the world, and ownership remains
-# with ZeOmega Inc The Software is protected by the copyright laws of
-# the United States and international copyright treaties.  Licensee is
-# not allowed to distribute the binary and source code (if released) to
-# third parties.  Licensee is not allowed to reverse engineer,
-# disassemble or decompile code, or make any modifications of the binary
-# or source code, remove or alter any trademark, logo, copyright or
-# other proprietary notices, legends, symbols, or labels in the
-# Software.  Licensee is not allowed to sub-license the Software or any
-# derivative work based on or derived from the Software.  Neither the
-# names of ZeOmega Inc , nor the names of its contributors may be used
-# to endorse or promote products derived from this Software without
-# specific prior written permission.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR
-# ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
-# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
-#
-#########################################################################
-##############################################################################
-#
-# Copyright (c) 2006 Zope Corporation and Contributors.
-# All Rights Reserved.
-#
-# This software is subject to the provisions of the Zope Public License,
-# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
-# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE.
-#
-##############################################################################
 """Bootstrap a buildout-based project
-
+python test file
 Simply run this script in a directory containing a buildout.cfg.
 The script accepts buildout command-line options, so you can
 use the -c option to specify an alternate configuration file.
